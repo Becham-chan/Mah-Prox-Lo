77726c342b2e0fb3ab0fbc82bd24aaa04c89ba54
BumpkinBeck <111549621+Becham-chan@users.noreply.github.com>
2024-06-06 23:00:57 +0700

    Add files via upload

--- PaintCountries.pl
+++ PaintCountries.pl
@@ -6,6 +6,18 @@ color(red).
 color(green).
 color(blue).
 
+% Add another color to show if coloring requires 4 colors.
+color(cyan).
+
 colorify(WA, NT, SA, Q, NSW, V, T):-
     color(WA), color(NT), color(SA), color(Q), color(NSW), color(V), color(T),
     \+ WA=NT, \+ WA=SA, \+NT=SA, \+NT=Q, \+SA=Q, \+SA=NSW, \+Q=NSW, \+SA=V, \+NSW=V, \+V=T.
+
+% Presume DEU = Germany, POL = Poland, CZE = Czech Republic,
+% SLV = Slovakia, AUS = Austria, HUN = Hungary, SLN = Slovenia,
+% SUI = Switzerland, ITA = Italy
+
+
+
+colorify(DEU, POL, CZE, SLV, AUS, HUN, SLN, SUI, ITA):-
+    color(DEU), color(POL), color(CZE), color(SLV), color(AUS), color(HUN), color(SLN), color(SUI), color(ITA), \+ DEU=POL, \+ DEU=CZE, \+DEU=SUI, \+DEU=AUS, \+POL=CZE, \+POL=SLV, \+CZE=SLV, \+CZE=AUS, \+SLV=AUS, \+SLV=HUN, \+AUS=SUI, \+AUS=HUN, \+AUS=SLN, \+AUS=ITA, \+HUN=SLN, \+SLN=ITA, \+SUI=ITA.
